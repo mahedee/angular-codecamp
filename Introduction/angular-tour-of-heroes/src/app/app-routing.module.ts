5ef664ca4b88b44837a102c7dc8a435064122bbc
mahedee <mahedee.hasan@gmail.com>
2019-01-28 13:19:50 +0600

    complete introduction

--- Introduction/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ Introduction/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+const routes: Routes = [
+  //Default route
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {path: 'dashboard', component: DashboardComponent},
+  
+  //The colon (:) in the path indicates that :id is a placeholder for a specific hero id.
+  {path:'detail/:id', component: HeroDetailComponent},
+  {path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    //The method is called forRoot() because you configure the router at the application's root level. 
+    //The forRoot() method supplies the service providers and directives needed for routing, 
+    //and performs the initial navigation based on the current browser URL.
+
+    //CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  // declarations: []
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Introduction/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Introduction/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,18 +8,42 @@ import {HeroService} from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  //heroes = HEROES;
   heroes : Hero[];
-  selectedHero : Hero;
+  //heroes = HEROES;
+  //selectedHero : Hero;  
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
+
+  getHeroes(): void {
+    //The JavaScript (+) operator converts the string to a number
+   // const id = +this.route.snapshot.paramMap.get('id');
+
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+    //this.heroes = this.heroService.getHeroes();
+  }
   
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
   /*
   hero : Hero = {
     id : 1,
@@ -27,18 +51,17 @@ export class HeroesComponent implements OnInit {
   };
 */
 
-  onSelect(hero : Hero) : void{
-    this.selectedHero = hero;
-  }
+  // onSelect(hero : Hero) : void{
+  //   this.selectedHero = hero;
+  // }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-    //this.heroes = this.heroService.getHeroes();
-  }
+ 
 
 
-
+// goBack() : void
+// {
+//   this.location.back();
+// }
 
 
 }
