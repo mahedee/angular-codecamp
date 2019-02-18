46d9e2473b6edc7c8225dec0d982a30982b03b1e
mahedee <mahedee.hasan@gmail.com>
2019-02-18 14:22:39 +0600

    complete chapter 08

---
+++ Angular-getting-started/Ch08/APM/src/app/products/product.ts
@@ -0,0 +1,38 @@
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    description: string;
+    price: number;
+    starRating: number;
+    imageUrl: string;
+}
+
+export class Product implements IProduct{
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    description: string;
+    price: number;
+    starRating: number;
+    imageUrl: string;
+    
+    constructor(productId: number,  
+    productName: string,
+    productCode: string,
+    releaseDate: string,
+    description: string,
+    price: number,
+    starRating: number,
+    imageUrl: string){
+
+    }
+
+    calculateDiscount(percent : number) : number{
+        return this.price - (this.price * percent / 100);
+    }
+
+
+}
